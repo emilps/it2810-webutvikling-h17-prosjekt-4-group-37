0115c73f34ec76fd587daf5a4f620d4ea678d851
Emil Schrøder <emilps@stud.ntnu.no>
2017-10-26 17:57:23 +0200

    new start with db

--- my-app/karma.conf.js
+++ my-app/karma.conf.js
@@ -22,13 +22,6 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
-    browsers: ['Chrome'],
-    customLaunchers: {
-      ChromeNoSandbox: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-        }
-    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
@@ -37,6 +30,4 @@ module.exports = function (config) {
     browsers: ['Chrome'],
     singleRun: false
   });
-
-
 };
