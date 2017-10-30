f37b28fc4ba8c899330240b7ab291cab0fe950b6
Emil Schrøder <emilps@stud.ntnu.no>
2017-10-30 16:20:47 +0100

    Added winesearch

---
+++ my-app/src/app/wine-search/wine-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wine-search',
+  templateUrl: './wine-search.component.html',
+  styleUrls: ['./wine-search.component.css']
+})
+export class WineSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
