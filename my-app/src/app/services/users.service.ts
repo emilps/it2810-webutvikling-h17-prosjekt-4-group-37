dca766c8542e2138d00e4e1ca4ffeeba89f388d8
oystpoyst <oystein.hammersland@gmail.com>
2017-10-31 14:05:22 +0100

    some minor changes to service. Added result atributte

--- my-app/src/app/services/users.service.ts
+++ my-app/src/app/services/users.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Http, URLSearchParams } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 import { User } from '../model/user';
 import 'rxjs/add/operator/map';
@@ -9,21 +8,26 @@ import 'rxjs/add/operator/catch';
 @Injectable()
 export class UserService {
 
+    result: any;
     constructor(private _http: Http) {
     }
 
-    insertNewUser(user:User): Observable<any>{
-      return this._http.post("/api/users", user)
-        .map((res:any) =>{
-          return res.json();
-        })
-        .catch((error:any) => {
-          return Observable.throw(error.json ? error.json().error : error || 'server error');
-        })
+    insertNewUser(user:User){
+      return this._http.post("/api/register", user)
+        //.map((res:any) =>{
+          //return res.json();
+        //})
+        //.catch((error:any) => {
+        //  return Observable.throw(error.json ? error.json().error : error || 'server error');
+        //})
     }
-    getUsers() {
-      return this._http.get("/api/getUser")
-        .map(result => this.result = result.json().data);
+
+    getUser(user:User){
+      return this._http.post("/api/getUser", user).map(result => this.result = result.json().data)
     }
+    //getUsers() {
+      //return this._http.get("/api/getUser")
+        //.map(result => this.result = result.json().data);
+    //}
 
 }
