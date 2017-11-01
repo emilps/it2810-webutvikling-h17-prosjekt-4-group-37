5d047ce1427105d29d596390dd469a97d94fbcac
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2017-11-01 15:14:44 +0100

    added a list to display wines

--- my-app/src/app/map/map.component.ts
+++ my-app/src/app/map/map.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+// Import the DataService
+import { DataService } from './../data.service';
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -7,7 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  // Define a users property to hold our user data
+  wines: Array<any>;
+
+  // Create an instance of the DataService through dependency injection
+  constructor(private _dataService: DataService) {
+
+    // Access the Data Service's getWines() method we defined
+    this._dataService.getWines()
+        .subscribe(res => this.wines = res);
+  }
 
   ngOnInit() {
   }
