c6bb906ab81a07b0d4aab485b3136b996d0aa613
Emil Schrøder <emilps@stud.ntnu.no>
2017-11-07 21:09:54 +0100

    Added loggincheck for favorite

--- my-app/src/app/single-wine/single-wine.component.ts
+++ my-app/src/app/single-wine/single-wine.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef } from '@angular/material';
+import { MatDialogRef, MatDialog } from '@angular/material';
 import { MAT_DIALOG_DATA } from '@angular/material';
 import {MatSnackBar} from '@angular/material';
+import { UserService } from './../services/users.service';
+import { LoginDialogComponent } from './../login-dialog/login-dialog.component';
 
 @Component({
   selector: 'app-single-wine',
@@ -12,25 +14,44 @@ export class SingleWineComponent implements OnInit {
 
   public icon = 'star_border';
   public position = 'above';
+  private userLoggedIn = false
 
-  constructor(public thisDialogRef: MatDialogRef<SingleWineComponent>, @Inject(MAT_DIALOG_DATA) public data: string, public snackBar: MatSnackBar) { }
+  constructor(
+    public thisDialogRef: MatDialogRef<SingleWineComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: string,
+    public snackBar: MatSnackBar,
+    private userService: UserService,
+    public dialog: MatDialog) {
+      this.userLoggedIn = this.userService.isLoggedIn()
+    }
 
   ngOnInit() {
   }
 
   changeIcon(wine){
-    console.log(wine)
-    if(this.icon == "star"){
-      this.icon = "star_border";
-      this.snackBar.open((wine + " er fjernet fra favoritter"),"OK", {
-        duration: 2000,
-      });
+    console.log(this.userLoggedIn)
+    if(this.userLoggedIn){
+      if(this.icon == "star"){
+        this.icon = "star_border";
+        this.snackBar.open((wine + " er fjernet fra favoritter"),"OK", {
+          duration: 2000,
+        });
+      }else{
+        this.icon = "star";
+        this.snackBar.open((wine + " er lagret i favoritter"),"OK", {
+          duration: 2000,
+        });
+      }
     }else{
-      this.icon = "star";
-      this.snackBar.open((wine + " er lagret i favoritter"),"OK", {
-        duration: 2000,
-      });
+      this.openDialog();
     }
+  }
+
+  openDialog(): void {
+    let dialogRef = this.dialog.open(LoginDialogComponent, {
+      width: '500px',
+      data: "Test tekst"
+    });
   }
 
   openSnackBar() {
