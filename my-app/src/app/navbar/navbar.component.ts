be4814b19df0f70234f841285cb169c016f6da1f
oystpoyst <oystein.hammersland@gmail.com>
2017-11-14 08:59:07 +0100

    Some hangling for navbar button change when logged in.

--- my-app/src/app/navbar/navbar.component.ts
+++ my-app/src/app/navbar/navbar.component.ts
@@ -1,18 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 import { LoginDialogComponent } from './../login-dialog/login-dialog.component';
+import { UserService} from '../services/users.service';
+import { User } from '../model/user';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  providers: [UserService]
+
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(public dialog: MatDialog) { }
+  public loggedInOptions = false;
 
-  ngOnInit() {
+  constructor(public dialog: MatDialog,private userService: UserService,){
+
+
+   }
+
+
+  async ngOnInit() {
+    await this.userService.fetchUserAsync()
+    if (this.userService.isLoggedIn()) {
+      this.loggedInOptions = true;
+    } else {
+      this.loggedInOptions = false;
+    }
   }
 
+  public async loggedInNavbar() {
+    await this.userService.fetchUserAsync()
+    if (this.userService.isLoggedIn()) {
+      this.loggedInOptions = true;
+    } else {
+      this.loggedInOptions = false;
+    }
+  }
 
   openDialog(): void {
     let dialogRef = this.dialog.open(LoginDialogComponent, {
@@ -22,4 +47,10 @@ export class NavbarComponent implements OnInit {
 
 
 }
+
+  logOut(){
+    console.log("Test for logout");
+    this.userService.logOutUser();
+  }
+
 }
