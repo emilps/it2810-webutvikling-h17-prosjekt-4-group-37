ef3a39b8b922aa7aab87794170029ccda2309503
oystpoyst <oystein.hammersland@gmail.com>
2017-11-17 18:55:00 +0100

    Nothing else than lining

--- my-app/src/app/services/favoritewine.service.ts
+++ my-app/src/app/services/favoritewine.service.ts
@@ -33,8 +33,8 @@ export class FavoriteWineService {
     getFavoriteWines() {
       return this._http.get("api/getFavoriteWinesIds")
       .map(result => this.result = result.json().data);
-
     }
+
 
 
 }
