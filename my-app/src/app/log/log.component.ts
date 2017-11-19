6aa1406f1e6b15d42bffc23461f6b3b51536e5fe
oystpoyst <oystein.hammersland@gmail.com>
2017-11-19 17:52:41 +0100

    Added a log component

--- my-app/src/app/log/log.component.ts
+++ my-app/src/app/log/log.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FavoriteWineService } from './../services/favoritewine.service';
 import { UserService } from './../services/users.service';
 import {ProfileService} from './../services/profile.service';
+import { SingleWineComponent } from './../single-wine/single-wine.component';
+import { MatDialog } from '@angular/material';
+
 
 @Component({
   selector: 'app-log',
@@ -10,11 +13,13 @@ import {ProfileService} from './../services/profile.service';
 })
 export class LogComponent implements OnInit {
 
+  showLog= true;
   wines: any;
   constructor(
     private favoriteWineService: FavoriteWineService,
     public userService: UserService,
-    private profileService: ProfileService
+    private profileService: ProfileService,
+    public dialog: MatDialog,
   ) { }
 
   ngOnInit() {
@@ -23,5 +28,18 @@ export class LogComponent implements OnInit {
 
   async gatherWinesLog(){
     this.wines = await this.profileService.getWinesLog()
+    if(this.wines.length){
+      this.showLog = false;
+    }
+  }
+
+  openDialog(arg){
+    console.log(arg)
+    let dialogRef = this.dialog.open(SingleWineComponent, {
+      //width: '600px',
+      data: arg,
+    })
+    dialogRef.afterClosed().subscribe(result => console.log(result))
+
   }
 }
