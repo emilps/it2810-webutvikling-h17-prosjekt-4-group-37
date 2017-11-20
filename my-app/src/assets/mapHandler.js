6ea2fc3cbf8061b41fbf0904ab43118931fa4408
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2017-11-20 17:57:53 +0100

    comment and cleanup of the mapHandlerjs code

--- my-app/src/assets/mapHandler.js
+++ my-app/src/assets/mapHandler.js
@@ -1,5 +1,12 @@
+/*
+Script that loads the clickable GeoChart Map in the map.component
+*/
+
+/* Sets a interval and checks if the div that should contain the Map
+excists, to avoid errors */
 let myInterval = setInterval(checkForMap, 1000);
 function checkForMap() {
+  //Checks if the current page cointains the div for the Map to load in
   if (document.getElementById("regions_div")) {
     clearInterval(myInterval);
     google.charts.load('current', {
@@ -9,9 +16,8 @@ function checkForMap() {
       'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
     });
     google.charts.setOnLoadCallback(drawRegionsMap);
-
-
-
+    /* Draws the Regionmap with our countries that have wines, and displays
+    the number of wines on mouse hover */
     function drawRegionsMap() {
       var data = google.visualization.arrayToDataTable([
         ['Country', 'Antall viner'],
@@ -42,48 +48,40 @@ function checkForMap() {
         ['Romania', 10],
         ['Czechia', 4]
       ]);
-
+      //Initial options to show the world continents
       var options = {
         resolution:'continents',
         colorAxis: {values: [1, 10, 100, 1000], colors: ['green', 'yellow', 'orange' ,'red'],},
         legend: 'none'
       };
-
+      //The chart that will be displayed and its position
       var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
-
+      //Card titles for the Map and Table
       let title = document.getElementById('regionTitle');
       let mapTitle = document.getElementById('mapTitle');
-
+      /* Add eventListener to change the view from continents to the countries
+      of the clicked continent. Add eventListener to change wines displayed
+      to the country clicked */
       function addListeners(){
+        //One time listener for the Continent click
         google.visualization.events.addOneTimeListener(chart, 'regionClick', function (eventData) {
+                  //Changes option 'region' to the clicked continent
                   options['region'] = eventData.region;
+                  //Changes option 'resolution' to show Map at country level
                   options['resolution'] = 'countries';
-
-                  /* Old code for changing Title to display selected continent
-                  switch(eventData.region) {
-                    case "150":
-                        title.innerHTML = "Europa";
-                        break;
-                    case "002":
-                        title.innerHTML = "Afrika";
-                        break;
-                    case "019":
-                        title.innerHTML = "Amerika";
-                        break;
-                    case "142":
-                        title.innerHTML = "Asia";
-                        break;
-                    default:
-                        title.innerHTML = eventData.region;
-                }
-                */
+                  //Changes Map Card Title to indicate new expected user behavior
                   mapTitle.innerHTML = "Velg land for å se nasjonens viner";
+                  //Draws new Map with the set options
                   chart.draw(data, options);
             });
+        //Listener for when a Country is clicked
         google.visualization.events.addListener(chart, 'select', function() {
           var selectedItem = chart.getSelection()[0];
           if (selectedItem) {
             var country = data.getValue(selectedItem.row, 0);
+              /* Switch to display selected country in Norwegian, not the
+              prettiest solution, but necessary since the chart only supports
+              English */
               switch(country) {
                 case "France":
                     title.innerHTML = "Frankrike";
@@ -169,16 +167,15 @@ function checkForMap() {
           }
         });
     }
+      //Default Map view drawing and listeners added
       addListeners();
       chart.draw(data, options);
-
-      //Adds eventListener to click on the "back" button to return to continent view on the map
+      //Adds eventListener to click on the "Tilbake" button to return to continent view on the Map
       var backButton = document.getElementById('backButton');
       backButton.addEventListener("click",function(e){
         options['region'] = 'world';
         options['resolution'] = 'continents';
         chart.draw(data, options);
-        title.innerHTML = "Verden";
         mapTitle.innerHTML = "Velg kontinent";
         addListeners();
       },false);
