e334fe7b0ddc1a352ce3565e1c338c856377d536
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2017-11-21 18:44:35 +0100

    chart tests fixed

--- my-app/src/app/chart/chart.component.spec.ts
+++ my-app/src/app/chart/chart.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { ProfileService } from './../services/profile.service';
+import { FavoriteWineService } from './../services/favoritewine.service';
+import { UserService} from './../services/users.service';
+import { HttpModule } from '@angular/http';
+
+import { ChartsModule } from 'ng2-charts';
 import { ChartComponent } from './chart.component';
+
+import { MatCardModule } from '@angular/material';
 
 describe('ChartComponent', () => {
   let component: ChartComponent;
@@ -8,14 +16,26 @@ describe('ChartComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChartComponent ]
+      declarations: [
+        ChartComponent
+      ],
+      imports: [
+        HttpModule,
+        MatCardModule,
+        ChartsModule,
+      ],
+      providers: [
+        ProfileService,
+        UserService,
+        FavoriteWineService,
+      ],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ChartComponent);
-    component = fixture.componentInstance;
+    component = fixture.debugElement.componentInstance;
     fixture.detectChanges();
   });
 
