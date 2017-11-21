913605b067c606fb7762131e27063eecf32f88e1
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2017-11-21 18:47:10 +0100

    fixed dataservice tests

--- my-app/src/app/data.service.spec.ts
+++ my-app/src/app/data.service.spec.ts
@@ -1,11 +1,23 @@
 import { TestBed, inject } from '@angular/core/testing';
-
 import { DataService } from './data.service';
+
+import { ProfileService } from './services/profile.service';
+import { FavoriteWineService } from './services/favoritewine.service';
+import { UserService} from './services/users.service';
+import { HttpModule } from '@angular/http';
 
 describe('DataService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [DataService]
+      declarations: [
+
+      ],
+      imports: [
+        HttpModule,
+      ],
+      providers: [
+        DataService
+      ],
     });
   });
 
