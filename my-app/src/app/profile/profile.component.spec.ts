b8bb1507060ffb0414129c2a98f49d0806726202
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2017-11-21 20:27:22 +0100

    fixed profile component tests

--- my-app/src/app/profile/profile.component.spec.ts
+++ my-app/src/app/profile/profile.component.spec.ts
@@ -1,6 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProfileComponent } from './profile.component';
+
+import { HttpModule } from '@angular/http';
+import { UserService } from '../services/users.service';
+import { User } from '../model/user';
+import { ProfileService } from './../services/profile.service';
+import { FavoriteWineService } from './../services/favoritewine.service';
+import { Filter } from './filter';
+import { SingleWineComponent } from './../single-wine/single-wine.component';
+import { ChartComponent } from './../chart/chart.component';
+import { UsersWinesComponent } from './../users-wines/users-wines.component';
+import { LogComponent } from './../log/log.component';
+import { ChartsModule } from 'ng2-charts';
+
+import { MatDialogModule, MatExpansionModule, MatIconModule, MatProgressSpinnerModule, MatTooltipModule, MatCardModule, MatFormFieldModule } from '@angular/material';
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
@@ -8,7 +21,29 @@ describe('ProfileComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProfileComponent ]
+      declarations: [
+        ProfileComponent,
+        SingleWineComponent,
+        ChartComponent,
+        UsersWinesComponent,
+        LogComponent,
+      ],
+      imports: [
+        HttpModule,
+        MatDialogModule,
+        MatExpansionModule,
+        MatCardModule,
+        MatIconModule,
+        MatProgressSpinnerModule,
+        MatTooltipModule,
+        MatFormFieldModule,
+        ChartsModule,
+      ],
+      providers: [
+        ProfileService,
+        UserService,
+        FavoriteWineService,
+      ],
     })
     .compileComponents();
   }));
