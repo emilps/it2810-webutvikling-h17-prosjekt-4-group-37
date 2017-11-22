25e88b1d61917a671796fd9e76f521273e8dc9db
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2017-11-22 10:28:12 +0100

    winesearch comp spec fix

--- my-app/src/app/wine-search/wine-search.component.spec.ts
+++ my-app/src/app/wine-search/wine-search.component.spec.ts
@@ -1,18 +1,69 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { WineSearchComponent } from './wine-search.component';
-
+//Import Router, Routes and Http modules
 import { HttpModule } from '@angular/http';
-import { FavoriteWineService } from './../services/favoritewine.service';
-import { UserService } from './../services/users.service';
+import { RouterModule, Routes } from '@angular/router';
+//Import our Components
+import { LoginComponent } from './../login/login.component';
+import { RegisterComponent } from './../register/register.component';
+import { NavbarComponent } from './../navbar/navbar.component';
+import { PageNotFoundComponent } from './../page-not-found/page-not-found.component';
+import { LoginDialogComponent } from './../login-dialog/login-dialog.component';
+import { LogComponent } from './../log/log.component';
+import { MapComponent } from './../map/map.component';
 import { SingleWineComponent } from './../single-wine/single-wine.component';
-import { Subscription } from 'rxjs/Subscription';
+import { ProfileComponent } from './../profile/profile.component';
+import { UsersWinesComponent } from './../users-wines/users-wines.component';
+import { ChartComponent } from './../chart/chart.component';
+//Import our Services
+import { ProfileService } from './../services/profile.service';
+import { FavoriteWineService } from './../services/favoritewine.service';
+import { UserService} from './../services/users.service';
+import { AuthGuard } from './../auth-guard.service';
 import { MessageService } from './../services/message.service';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DataService } from './../data.service';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
-import { MatCardModule, MatProgressSpinnerModule, MatIconModule, MatTooltipModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatDialogModule, MatSnackBarModule, MatExpansionModule, MatCheckboxModule } from '@angular/material';
-
+//Import Chart for doughnut/pie chart
+import { ChartsModule } from 'ng2-charts';
+//Import Angular Material Modules, Forms and Animations
+import { MatCardModule, MatTooltipModule , MatDialogModule, MatDialogRef, MAT_DIALOG_DATA, MatMenuModule, MatExpansionModule, MatCheckboxModule, MatFormFieldModule, MatTableModule, MatProgressSpinnerModule, MatSnackBarModule, MatSelectModule, MatInputModule, MatIconModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//Import Base URL
+import { APP_BASE_HREF } from '@angular/common';
+//MatDialogRef Mock
+class MdDialogRefMock {
+}
+//Application Routes
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: WineSearchComponent,
+  },
+  {
+    path: 'navbar',
+    component: NavbarComponent,
+    canActivate: [AuthGuard]
+    //Not full functional yet. Make it async maybe?
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'map',
+    component: MapComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  },
+];
+/* Test setup and execution */
 describe('WineSearchComponent', () => {
   let component: WineSearchComponent;
   let fixture: ComponentFixture<WineSearchComponent>;
@@ -48,13 +99,11 @@ describe('WineSearchComponent', () => {
     })
     .compileComponents();
   }));
-
   beforeEach(() => {
     //fixture = TestBed.createComponent(WineSearchComponent);
     //component = fixture.debugElement.componentInstance;
     //fixture.detectChanges();
   });
-
   it('should create', () => {
     //expect(component).toBeTruthy();
   });
