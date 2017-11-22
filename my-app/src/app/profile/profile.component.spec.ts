64a961272e564a7ff1afb51eee34f1e45815aa07
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2017-11-22 10:00:57 +0100

    pagenotfound and profile spec update

--- my-app/src/app/profile/profile.component.spec.ts
+++ my-app/src/app/profile/profile.component.spec.ts
@@ -1,24 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProfileComponent } from './profile.component';
-
+//Import Http Module
 import { HttpModule } from '@angular/http';
-import { UserService } from '../services/users.service';
-import { User } from '../model/user';
-import { ProfileService } from './../services/profile.service';
-import { FavoriteWineService } from './../services/favoritewine.service';
-import { Filter } from './filter';
+//Import our Components
 import { SingleWineComponent } from './../single-wine/single-wine.component';
 import { ChartComponent } from './../chart/chart.component';
 import { UsersWinesComponent } from './../users-wines/users-wines.component';
 import { LogComponent } from './../log/log.component';
+//Import our Services
+import { UserService } from '../services/users.service';
+import { ProfileService } from './../services/profile.service';
+import { FavoriteWineService } from './../services/favoritewine.service';
+//Import User and Filter
+import { User } from '../model/user';
+import { Filter } from './filter';
+//Import Chart Module for doughnut/pie chart
 import { ChartsModule } from 'ng2-charts';
-
+//Import Angular Material Modules
 import { MatDialogModule, MatExpansionModule, MatIconModule, MatProgressSpinnerModule, MatTooltipModule, MatCardModule, MatFormFieldModule } from '@angular/material';
-
+/* Test setup and execution */
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -47,13 +50,11 @@ describe('ProfileComponent', () => {
     })
     .compileComponents();
   }));
-
   beforeEach(() => {
     fixture = TestBed.createComponent(ProfileComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
