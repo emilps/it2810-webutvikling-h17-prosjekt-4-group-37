6c11e2899b188c5f8ce9d0937a3e8ed9a186f19d
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2017-11-22 09:56:57 +0100

    map and navbar spec

--- my-app/src/app/map/map.component.spec.ts
+++ my-app/src/app/map/map.component.spec.ts
@@ -1,14 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MapComponent } from './map.component';
-
-import { MatTableModule, MatCardModule, MatTooltipModule, MatDialogModule } from '@angular/material';
-import { MapWineService } from './../services/mapwine.service';
+//Import Http Module
 import { HttpModule } from '@angular/http';
-
+//Import our Service
+import { MapWineService } from './../services/mapwine.service';
+//Import Angular Material Module
+import { MatTableModule, MatCardModule, MatTooltipModule, MatDialogModule } from '@angular/material';
+/* Test setup and execution */
 describe('MapComponent', () => {
   let component: MapComponent;
   let fixture: ComponentFixture<MapComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -27,13 +28,11 @@ describe('MapComponent', () => {
     })
     .compileComponents();
   }));
-
   beforeEach(() => {
     //fixture = TestBed.createComponent(MapComponent);
     //component = fixture.debugElement.componentInstance;
     //fixture.detectChanges();
   });
-
   it('should create', () => {
     //expect(component).toBeTruthy();
   });
