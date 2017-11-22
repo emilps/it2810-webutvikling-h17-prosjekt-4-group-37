7e731aea8831bf75a66f0c412f02e464f3343484
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2017-11-22 10:40:16 +0100

    merge test with master

--- my-app/src/app/services/data.service.spec.ts
+++ my-app/src/app/services/data.service.spec.ts
@@ -1,17 +1,16 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { DataService } from './data.service';
-
+//Import Http Module
+import { HttpModule } from '@angular/http';
+//Import our Services
 import { ProfileService } from './services/profile.service';
 import { FavoriteWineService } from './services/favoritewine.service';
 import { UserService} from './services/users.service';
-import { HttpModule } from '@angular/http';
-
+/* Test setup and execution */
 describe('DataService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [
-
-      ],
+      declarations: [],
       imports: [
         HttpModule,
       ],
@@ -20,7 +19,6 @@ describe('DataService', () => {
       ],
     });
   });
-
   it('should be created', inject([DataService], (service: DataService) => {
     expect(service).toBeTruthy();
   }));
