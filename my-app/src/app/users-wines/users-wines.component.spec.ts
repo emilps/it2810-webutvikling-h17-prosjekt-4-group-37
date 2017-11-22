ab239cb3285c88d922ddf1eb8e8a9619960fd46e
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2017-11-22 10:20:32 +0100

    singlewine and userswine spec fix

--- my-app/src/app/users-wines/users-wines.component.spec.ts
+++ my-app/src/app/users-wines/users-wines.component.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { UsersWinesComponent } from './users-wines.component';
-
+//Import Http Module
 import { HttpModule } from '@angular/http';
+//Import our Component(s)
+import { SingleWineComponent } from './../single-wine/single-wine.component';
+//Import our Services
 import { FavoriteWineService } from './../services/favoritewine.service';
 import { UserService } from './../services/users.service';
-import { SingleWineComponent } from './../single-wine/single-wine.component';
-import { Subscription } from 'rxjs/Subscription';
 import { MessageService } from './../services/message.service';
-
+//Import Angular Material Modules
 import { MatCardModule, MatProgressSpinnerModule, MatIconModule, MatTooltipModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatDialogModule, MatSnackBarModule } from '@angular/material';
-
+/* Test setup and execution */
 describe('UsersWinesComponent', () => {
   let component: UsersWinesComponent;
   let fixture: ComponentFixture<UsersWinesComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -40,13 +40,11 @@ describe('UsersWinesComponent', () => {
     })
     .compileComponents();
   }));
-
   beforeEach(() => {
     fixture = TestBed.createComponent(UsersWinesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
