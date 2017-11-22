6ac814336f81c0150cc09abcefa35d43df8cb3a7
Emil Schrøder <emilps@stud.ntnu.no>
2017-11-22 20:42:57 +0100

    Tests for 404

--- my-app/src/app/page-not-found/page-not-found.component.spec.ts
+++ my-app/src/app/page-not-found/page-not-found.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { PageNotFoundComponent } from './page-not-found.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 /* Test setup and execution */
 describe('PageNotFoundComponent', () => {
   let component: PageNotFoundComponent;
@@ -15,7 +17,17 @@ describe('PageNotFoundComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-  it('should create', () => {
+  it('should create 404 page', () => {
     expect(component).toBeTruthy();
+  });
+  it('should create 404 text', () => {
+    let de = fixture.debugElement.query(By.css('.front'));
+    let el = de.nativeElement;
+    expect(el.textContent).toContain('404');
+  });
+  it('Image should load', () => {
+    let de = fixture.debugElement.query(By.css('img'));
+    let el = de.nativeElement;
+    expect(el.alt).toContain('drunk404');
   });
 });
